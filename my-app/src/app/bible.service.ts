a9d37d59a8566cfe1f4dfe3c3b21f64cbe8baff0
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-07-16 15:35:48 -0400

    update

--- my-app/src/app/bible.service.ts
+++ my-app/src/app/bible.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import books from '../assets/kjv/Books.json';
+import Genesis from '../assets/kjv/Genesis.json';
+import Exodus from '../assets/kjv/Exodus.json';
+import John from '../assets/kjv/John.json';
 
-let log = false;
+let log = true;
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,41 @@ export class BibleService {
   constructor() { }
 
   getBooks() {
-    if (log) console.log(BibleService.name + '.' + this.getBooks.name, {books});
+    if (log) console.log(BibleService.name + '.' + this.getBooks.name, { books });
     return books;
+  }
+
+  getBook(bookName): any {
+    console.log(this.getBook.name, {bookName});
+    let book;
+    if (bookName === 'Genesis') {
+      book = Genesis;
+    } else if (bookName === 'Exodus') {
+      book = Exodus;
+    } else if (bookName === 'John') {
+      book = John;
+    } else {
+      throw new Error('not implemented');
+    }
+    return book;
+  }
+
+  getVerses(bookName, chapter) {
+    let book = this
+      .getBook(bookName);
+    let verses = book
+      .chapters
+      .filter(c => c.chapter === chapter)[0];
+    console.log(BibleService.name + '.' + this.getVerses.name, {
+      bookName,
+      chapter,
+      verses,
+      book,
+    });
+    return verses.verses.map(v => v.verse);
+  }
+
+  getChapters(bookName) {
+    return this.getBook(bookName).chapters.map(c => c.chapter);
   }
 }
--- my-app/src/app/verse/verse.component.ts
+++ my-app/src/app/verse/verse.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { BibleService } from '../bible.service';
 
-let log = false;
+let log = true;
 
 @Component({
   selector: 'app-verse',
@@ -11,15 +11,37 @@ let log = false;
 })
 export class VerseComponent implements OnInit {
   form = new FormGroup({
-    selectedBook: new FormControl('')
+    selectedBook: new FormControl(''),
+    selectedChapter: new FormControl(''),
+    selectedVerse: new FormControl(''),
   });
   books;
+  verses = [];
+  chapters = [];
 
   constructor(private bible: BibleService) {
-    if (log) console.log(VerseComponent.name, {bible});
+    if (log) console.log(VerseComponent.name+'.ctor', {bible});
     this.books = bible.getBooks();
   }
 
   ngOnInit(): void {
+    this.form.controls.selectedBook.setValue('Genesis');
+    this.form.controls.selectedChapter.setValue('1');
+    this.form.controls.selectedVerse.setValue('1');
+    this.update();
+    this.form.valueChanges.subscribe(val => {
+      this.update();
+    });
+  }
+
+  update() {
+    let controls = this.form.controls;
+    this.chapters = this.bible.getChapters(controls.selectedBook.value);
+    this.verses = this.bible.getVerses(controls.selectedBook.value, controls.selectedChapter.value || '1');
+    if (log) console.log(VerseComponent.name+'.'+this.update.name, {
+      val: controls,
+      'this.chapters':this.chapters,
+      'this.verses':this.verses,
+    });
   }
 }
